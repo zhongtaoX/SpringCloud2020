d3b9e9e74a2e2112828fed5feb1b546db3d72525
zhongtao <18269776130@163.com>
2023-03-30 00:21:52 +0800

    build(SpringCloud2020项目搭建bug修复): SpringCloud2020项目搭建+Eureka注册中心等bug修复

--- cloud_consumer_order80/src/main/java/com/zt/springcloud/config/ApplicationContextConfig.java
+++ cloud_consumer_order80/src/main/java/com/zt/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.zt.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig {
 
     @Bean
+//    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- cloud_consumer_order80/src/main/java/com/zt/springcloud/controller/OrderController.java
+++ cloud_consumer_order80/src/main/java/com/zt/springcloud/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.zt.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,16 +16,17 @@ import javax.annotation.Resource;
 public class OrderController {
 
     public static final String PAYMENT_URL = "http://localhost:8001";
+//    public static final String PAYMENT_URL = "http://CLOUD_PROVIDER_PAYMENT";
 
     @Resource
     private RestTemplate restTemplate;
 
-    @GetMapping("/consumer/payment/create")
+    @GetMapping(value = "/consumer/payment/create")
     public CommonResult<Payment> creat(Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
-    @GetMapping("/consumer/payment/get/{id}")
+    @GetMapping(value = "/consumer/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
     }
