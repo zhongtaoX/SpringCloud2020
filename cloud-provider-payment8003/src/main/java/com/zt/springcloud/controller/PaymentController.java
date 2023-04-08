28e8120067aa05a2e28b61975f0e39642260a9cb
zhongtao <18269776130@163.com>
2023-04-08 19:36:58 +0800

    feat(添加8003模块): 添加8003模块+zookeeper

---
+++ cloud-provider-payment8003/src/main/java/com/zt/springcloud/controller/PaymentController.java
@@ -0,0 +1,75 @@
+package com.zt.springcloud.controller;
+
+import com.zt.springcloud.entities.CommonResult;
+import com.zt.springcloud.entities.Payment;
+import com.zt.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult creat(@RequestBody Payment payment) {
+
+        int result = paymentService.create(payment);
+        log.info("插入结果：" + result);
+
+        if (result > 0) {
+            return new CommonResult<>(200, "实体创建成功", result);
+        } else {
+            return new CommonResult<>(444, "实体创建失败", null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("插入结果：" + payment);
+
+        if (payment != null) {
+            return new CommonResult<>(200, "查询成功，server.port" + serverPort, payment);
+        } else {
+            return new CommonResult<>(444, "无数据", null);
+        }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info(String.valueOf(element));
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PROVIDER-PAYMENT");
+        for (ServiceInstance instance : instances) {
+            log.info(String.valueOf(instance));
+        }
+
+        return this.discoveryClient;
+    }
+
+    @GetMapping(value = "/payment/zk")
+    public String paymentZookeeper() {
+        return "zookeeper: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
---
+++ cloud-provider-payment8003/src/main/java/com/zt/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.zt.springcloud.service.impl;
+
+import com.zt.springcloud.dao.PaymentDao;
+import com.zt.springcloud.entities.Payment;
+import com.zt.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
