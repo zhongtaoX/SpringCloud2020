2040bc5c48097eeefb8eb1cd640eac23ff0545c6
zhongtao <18269776130@163.com>
2023-04-10 23:52:42 +0800

    feat(恢复Eureka服务): 恢复Eureka服务，测试自带Ribbon负载均衡，IRul接口

--- cloud-consumer-order80/src/main/java/com/zt/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/zt/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.zt.springcloud.controller;
 import com.zt.springcloud.entities.CommonResult;
 import com.zt.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +26,24 @@ public class OrderController {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
+    @GetMapping(value = "/consumer/payment/createEntity")
+    public CommonResult<Payment> creatEntity(Payment payment) {
+        return restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class).getBody();
+    }
+
     @GetMapping(value = "/consumer/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping(value = "/consumer/payment/getEntity/{id}")
+    public CommonResult<Payment> getPaymentByIdEntity(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败 ");
+        }
     }
 }
--- cloud-eureka-server7001/src/main/java/com/zt/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/zt/springcloud/EurekaMain7001.java
@@ -2,10 +2,10 @@ package com.zt.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-//@EnableEurekaServer
+@EnableEurekaServer
 public class EurekaMain7001 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaMain7001.class, args);
